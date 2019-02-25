4f945f540826320b3470341d486437fe1f2331c7
Connor Burri <cjburri@wpi.edu>
2019-02-25 14:45:41 -0500

    Added a plan of attack and implemented the plan into the code

--- caeserCypher.c
+++ caeserCypher.c
@@ -3,6 +3,14 @@
  * Date: 2/25/19 
  */
  
+ void printCypher();
+ int translatePhrase();
+ int takeInput();
+ 
+ char* inputString; //the string that will be put through the cypher
+ char* outputCypher; //the outputted cypher
+ int translationVariable;
+ 
  int main(){
  	
  	
